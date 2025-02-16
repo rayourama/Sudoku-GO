6ef6f6d794436a8ca4ae0cc991892e6fc15ec72d
Ryan Ramassamy <rayourama@hotmail.com>
2025-02-17 00:45:54 +0100

    Implémentation de la grille

--- app/main.go
+++ app/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 // Dimensions de la grille
 const (
 	screenWidth  = 900
 	screenHeight = 900
+	cellSize     = screenWidth / 9
 )
 
 // Game structure
@@ -23,8 +25,22 @@ func (g *Game) Update() error {
 
 // Interface graphique
 func (g *Game) Draw(screen *ebiten.Image) {
-	texte := "Bienvenue dans Sudoku en Go!"
-	ebitenutil.DebugPrint(screen, texte)
+	// Couleurs
+	bgColor := color.RGBA{240, 240, 240, 255} // Gris clair
+	screen.Fill(bgColor)
+	lineColor := color.RGBA{0, 0, 0, 255}
+
+	// Dessiner la grille 9x9
+	for i := 0; i <= 9; i++ {
+		thickness := 1.0
+		if i%3 == 0 {
+			thickness = 3.0 // Traits plus épais pour séparer les blocs 3x3
+		}
+		// Lignes verticales
+		vector.StrokeLine(screen, float32(i*cellSize), 0, float32(i*cellSize), screenHeight, float32(thickness), lineColor, true)
+		// Lignes horizontales
+		vector.StrokeLine(screen, 0, float32(i*cellSize), screenWidth, float32(i*cellSize), float32(thickness), lineColor, true)
+	}
 }
 
 // Layout gère la disposition de l'écran
@@ -38,7 +54,7 @@ func main() {
 
 	// Configuration de la fenêtre
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Sudoku en Go")
+	ebiten.SetWindowTitle("Sudoku")
 
 	// Lancement du jeu
 	if err := ebiten.RunGame(game); err != nil {
